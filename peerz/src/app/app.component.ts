e71e5d60c9ebf66fe8e99f3bad2188683778dd99
Ian Peter Tecuico <ipstecuico04@hotmail.com>
2020-07-02 09:06:11 +0800

    Added logout

--- peerz/src/app/app.component.ts
+++ peerz/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 import { AuthService } from './user/auth.service';
 
@@ -10,7 +11,8 @@ import { AuthService } from './user/auth.service';
 export class AppComponent {
   title = 'Peerz';
 
-  constructor(private authService: AuthService){}
+  constructor(private authService: AuthService,
+              private router: Router){}
 
   get isLoggedIn(): boolean {
     return this.authService.isLoggedIn;
@@ -21,5 +23,10 @@ export class AppComponent {
       return this.authService.currentUser.userName;
     }
     return '';
+  }
+
+  logOut(): void {
+    this.authService.logout();
+    this.router.navigateByUrl('/');
   }
 }
