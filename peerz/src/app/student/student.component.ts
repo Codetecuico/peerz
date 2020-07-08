7616c3c963638d5c7490bfa03a26de5ef4c98db0
Ian Peter Tecuico <ipstecuico04@hotmail.com>
2020-07-08 15:49:14 +0800

    Added search box

--- peerz/src/app/student/student.component.ts
+++ peerz/src/app/student/student.component.ts
@@ -23,11 +23,31 @@ export class StudentComponent implements OnInit {
       next: students => {
         this.students = students;
         this.filteredStudents = students;
+
+        this.filteredStudents.sort((a, b) => a.lastName.localeCompare(b.lastName))
       },
       error: err => {
         this.errorMessage = err;
       }
     });
+  }
+
+  _listFilter = '';
+  get listFilter(): string {
+    return this._listFilter;
+  }
+  set listFilter(value: string) {
+    this._listFilter = value;
+    this.filteredStudents = this.listFilter ? this.performFilter(this.listFilter) : this.students;
+  }
+
+  performFilter(filterBy: string): IStudent[] {
+    filterBy = filterBy.toLocaleLowerCase();
+
+    return this.students.filter((student: IStudent) =>
+        student.lastName.toLocaleLowerCase().indexOf(filterBy) !== -1
+        || student.firstName.toLocaleLowerCase().indexOf(filterBy) !== -1
+        )
   }
 
 }
